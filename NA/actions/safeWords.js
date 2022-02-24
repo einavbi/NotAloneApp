4c94d392f5eb9349f681af5e9bd117341a99ab95
Einav Bitton <57327571+einavbi@users.noreply.github.com>
2022-02-24 20:47:35 +0200

    Update safeWords.js

--- NA/actions/safeWords.js
+++ NA/actions/safeWords.js
@@ -1,9 +1,9 @@
 export const addSafeWords = (userId,word1=null,word2=null,word3=null) => {
     
     let Data=getSafeWords(userId)
-    if (word1==null){name3=Data.Word1}
-    if (word2==null){name1=Data.Word2}
-    if (word3==null){name2=Data.Word3}
+    if (word1==null){word1=Data.Word1}
+    if (word2==null){word2=Data.Word2}
+    if (word3==null){word3=Data.Word3}
 
     const response =  fetch(
     `https://notaloneapp-9aecd-default-rtdb.asia-southeast1.firebasedatabase.app/users/${userId}/safeWords.json`,
